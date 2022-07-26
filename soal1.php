075105acbd691f270e6393aa9091808d823f8de6
helmaFerani <helma_165@smkassalaambandung.sch.id>
2022-07-26 11:50:16 +0700

    ujian-pkl

---
+++ soal1.php
@@ -0,0 +1,10 @@
+<?php
+
+$i = 0;
+while (1,1,0,1,1,1) {
+    echo "Perulangan ke-{$i} <br>";
+
+    $i++;
+}
+
+  
---
+++ soal2.php
@@ -0,0 +1,12 @@
+<?php
+ 
+$number ();
+ 
+if(floor($number) == $number) {
+    $fraction->setNumerator((int)$number);
+} else {
+    echo 'Did not set the numerator.';
+    doSomethingElse();
+}
+ 
+?>
